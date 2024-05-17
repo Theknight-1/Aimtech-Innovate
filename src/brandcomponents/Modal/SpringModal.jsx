7f702b9ebdf4609a36d1f2a48d423d6e28f17076
Theknight <himanshusaxena5005@gmail.com>
2024-05-17 19:10:52 +0530

    ui updated

--- src/brandcomponents/Modal/SpringModal.jsx
+++ src/brandcomponents/Modal/SpringModal.jsx
@@ -293,7 +293,6 @@ const ExampleWrapper = ({ isOpen, setIsOpen, selectedOption }) => {
                     onChange={handleInputChange}
                     className="block py-2.5 px-0 w-full text-lg text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=""
-                    required
                   />
                   <label
                     htmlFor="floating_company_name"
